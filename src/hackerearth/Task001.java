b5ff2123659805c213590153a2cbe15e00c862ae
Armand Stanczak <armandstanczak@gmail.com>
2020-07-07 14:41:52 +0200

    HackerEarth tasks

---
+++ src/hackerearth/Task001.java
@@ -0,0 +1,16 @@
+package hackerearth;
+
+/**
+ *
+ * @author Armand Stańczak
+ */
+public class Task001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
